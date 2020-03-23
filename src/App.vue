1b735279e0228ebad05950a4a3ce56283366c76b
Jreake <Jreake@163.com>
2020-03-23 11:35:11 +0800

    storage

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,33 @@
 <template>
   <div id="app">
-   <router-view />
+    <router-view />
   </div>
 </template>
 
 <script>
+import storage from './storage/index'
 export default {
   name: 'App',
- 
+  data () {
+    return {
+
+    }
+  }, mounted () {
+    // storage.setItem("aa", 1)
+    //  storage.setItem('user', {
+    //    'a':'1',
+    //    'b':'222'
+    //  })
+    //  storage.setItem('abc', {
+    //    'a':'1',
+    //    'b':'222'
+    //  },'user')
+
+  }
+
+
 }
 </script>
 
 <style>
-
 </style>
